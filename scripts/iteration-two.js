9de7e9633b89b792cc26c00c6e9ad07988b6404f
EricMTodd <eterictodd@gmail.com>
2020-08-18 19:49:21 -0600

    Currently stuck with function logic and JavaScript best practices. Moving on with TOP in order to learn more.

--- scripts/iteration-two.js
+++ scripts/iteration-two.js
@@ -1,3 +1,38 @@
-function main() {
-  console.log("Hello world!");
+let round = 0;
+let computerScore = 0;
+let playerScore = 0;
+let computerChoice;
+let playerChoice;
+
+function selectRock() {
+  playerChoice = "rock";
+  console.log(`playerChoice: ${playerChoice}`);
+  getComputerChoice();
+}
+
+function selectPaper() {
+  playerChoice = "paper";
+  console.log(`playerChoice: ${playerChoice}`);
+  getComputerChoice();
+}
+
+function selectScissors() {
+  playerChoice = "scissors";
+  console.log(`playerChoice: ${playerChoice}`);
+  getComputerChoice();
+}
+
+function getComputerChoice() {
+  let computerChoice = Math.floor(Math.random() * 3);
+
+  if (computerChoice == 0) {
+    computerChoice = "rock";
+  } else if (computerChoice == 1) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";
+  }
+
+  console.log(`computerChoice: ${computerChoice}`);
+  return (computerChoice = computerChoice);
 }
